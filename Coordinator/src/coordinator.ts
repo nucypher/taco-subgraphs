7f9cde803332a18b27ad3d72a503983755a1ed02
Manuel Montenegro <manuel@nucypher.com>
2024-03-13 12:31:01 +0100

    Add RitualCounter entity

--- Coordinator/src/coordinator.ts
+++ Coordinator/src/coordinator.ts
@@ -4,34 +4,46 @@ import {
   StartRitual as StartRitualEvent,
   // EndRitual as EndRitualEvent,
 } from "../generated/Coordinator/Coordinator";
-import { Ritual } from "../generated/schema";
+import { Ritual, RitualCounter } from "../generated/schema";
 
 export function handleStartRitual(event: StartRitualEvent): void {
-  const ritual = Coordinator.bind(event.address).rituals(event.params.ritualId);
+  const ritualInst = Coordinator.bind(event.address).rituals(
+    event.params.ritualId,
+  );
 
-  const entity = new Ritual(event.params.ritualId.toString());
-  entity.initiator = ritual.getInitiator();
-  entity.initTimestamp = ritual.getInitTimestamp();
-  entity.endTimestamp = ritual.getEndTimestamp();
-  entity.authority = event.params.authority;
-  entity.dkgSize = ritual.getDkgSize();
-  entity.threshold = ritual.getThreshold();
-  entity.accessController = ritual.getAccessController();
-  entity.participants = changetype<Bytes[]>(event.params.participants);
-  entity.status = "DKG_AWAITING_TRANSCRIPTS";
-  entity.save();
+  const ritual = new Ritual(event.params.ritualId.toString());
+  ritual.initiator = ritualInst.getInitiator();
+  ritual.initTimestamp = ritualInst.getInitTimestamp();
+  ritual.endTimestamp = ritualInst.getEndTimestamp();
+  ritual.authority = event.params.authority;
+  ritual.dkgSize = ritualInst.getDkgSize();
+  ritual.threshold = ritualInst.getThreshold();
+  ritual.accessController = ritualInst.getAccessController();
+  ritual.participants = changetype<Bytes[]>(event.params.participants);
+  ritual.status = "DKG_AWAITING_TRANSCRIPTS";
+  ritual.save();
+
+  let ritualCounter = RitualCounter.load("Counter");
+  if (!ritualCounter) {
+    ritualCounter = new RitualCounter("Counter");
+    ritualCounter.total = 0;
+    ritualCounter.active = 0;
+  }
+
+  ritualCounter.total = ritualCounter.total + 1;
+  ritualCounter.save();
 }
 
 // export function handleEndRitual(event: EndRitualEvent): void {
-  // const ritualId = event.params.ritualId.toString();
-  // let entity = Ritual.load(ritualId);
-  // if (!entity) {
-  //   log.warning("Received EndRitual of unknown ritual ID: {}", [ritualId]);
-  //   entity = new Ritual(ritualId);
-  // }
-  // entity.endTime = event.block.timestamp;
-  // // entity.status = event.params.successful
-  // //   ? "SuccessfullyEnded"
-  // //   : "UnsuccessfullyEnded";
-  // entity.save();
+// const ritualId = event.params.ritualId.toString();
+// let entity = Ritual.load(ritualId);
+// if (!entity) {
+//   log.warning("Received EndRitual of unknown ritual ID: {}", [ritualId]);
+//   entity = new Ritual(ritualId);
+// }
+// entity.endTime = event.block.timestamp;
+// // entity.status = event.params.successful
+// //   ? "SuccessfullyEnded"
+// //   : "UnsuccessfullyEnded";
+// entity.save();
 // }
